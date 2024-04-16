02098ab8c1d1d8f404c9d8b311b16b2a5261e1ba
stlp <ctu4@uw.edu>
2024-04-15 22:01:16 -0700

    Completed Swift Store

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -6,19 +6,89 @@
 //
 
 import Foundation
+import XCTest
 
-protocol SKU {}
+protocol SKU {
+    var name: String {get}
+    
+    func price() -> Int
+}
 
-class Item {}
+class Item: SKU {
+    var name: String
+    var priceEach: Int
+    
+    init(name: String, priceEach: Int) {
+        self.name = name
+        self.priceEach = priceEach
+    }
+    
+    func price() -> Int {
+        return priceEach
+    }
+}
 
-class Receipt {}
+class Receipt {
+    var scannedItems: [SKU] = []
+    
+    func add(item: SKU) {
+        scannedItems.append(item)
+    }
+    
+    func items() -> [SKU] {
+        return scannedItems
+    }
+    
+    func total() -> Int {
+        var total = 0
+        for i in self.scannedItems {
+            total += i.price()
+        }
+        return total
+    }
+    
+    func output() -> String {
+        var outString = "Receipt:\n"
+        for i in self.scannedItems {
+            outString += "\(i.name): $\(Double(i.price())/100)\n"
+        }
+        outString += "------------------\n"
+        outString += "TOTAL: $\(Double(self.total())/100)"
+        return outString
+    }
+}
 
-class Register {}
+class Register {
+    var currentReceipt: Receipt = Receipt()
+    
+    func scan(_ item: SKU) {
+        currentReceipt.add(item: item)
+    }
+    
+    func subtotal() -> Int {
+        return currentReceipt.total()
+    }
+    
+    func total() -> Receipt{
+        var reciept = currentReceipt
+        currentReceipt = Receipt()
+        return reciept
+    }
+}
 
 class Store {
     let version = "0.1"
     func helloWorld() -> String {
         return "Hello world"
+    }
+}
+
+final class ItemTest: XCTestCase {
+    var register = Register()
+    
+    func testItem() {
+        register.scan(Item(name: "Apple", priceEach: 500))
+        XCTAssertEqual(500, register.subtotal())
     }
 }
 
